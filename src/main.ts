a48ba254ce40839ee9d26100f123ba312d1a8f21
louispawaon <lpawaon@gmail.com>
2024-03-03 21:17:48 +0800

    chore: added swagger and hot reloading

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Swagger Documentation
+  const config = new DocumentBuilder()
+    .setTitle('SmartCampus API')
+    .setDescription('The official SmartCampus API Documentation')
+    .setVersion('1.0')
+    .addTag('smartcampus')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //Hot-Reloading
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
   await app.listen(3000);
 }
 bootstrap();
