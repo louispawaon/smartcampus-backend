065dd9659ddb5a8ed72a48ad7e399abf57f75706
louispawaon <lpawaon@gmail.com>
2024-03-10 21:34:34 +0800

    refactor(usersService): added service for user creation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { AuthDto } from 'src/auth/dto/auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  extractUsername(email: string): string {
+    return email.split('@')[0];
+  }
+
+  async createUser(signUpDto: AuthDto) {
+    const { email, password, role } = signUpDto;
+    const username = this.extractUsername(email);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        password,
+        role,
+        username,
+      },
+    });
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+}
