700ee4665b2e4e34a5ec5b8d87f9958fdd204670
louispawaon <lpawaon@gmail.com>
2024-03-12 04:10:10 +0800

    chore(userService): added providers and import - import JwtModule - provider AuthGuard and JwtService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthGuard, JwtService],
 })
 export class UsersModule {}
