43ec100432e55829ae2bf23d5afa7f98e4c8fe06
louispawaon <lpawaon@gmail.com>
2024-03-12 20:20:29 +0800

    chore(auth): added swagger documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,9 @@ import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { Response } from 'express';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -10,15 +13,25 @@ export class AuthController {
     private readonly authService: AuthService,
   ) {}
 
+  @ApiOperation({
+    summary:
+      'Signs in existing users, creates an account and signs in if no existing account',
+  })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Successful sign-in' })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Invalid credentials or role',
+  })
   @Post()
   async signIn(@Body() authDto: AuthDto, @Res() res: Response) {
     try {
       const result = await this.authService.signIn(authDto, res);
       return result;
     } catch (error) {
+      console.log(error);
       if (error.message === 'Invalid email or password!') {
         await this.userService.createUser(authDto);
-        return this.signIn(authDto, res);
+        return this.authService.signIn(authDto, res);
       } else if (error.message === 'Invalid role!') {
         return res.status(HttpStatus.UNAUTHORIZED).json({
           message: 'Invalid Role!',
