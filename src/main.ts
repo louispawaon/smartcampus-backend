471bd6d33a66698829885b1b27a076bb5cfc3e42
louispawaon <lpawaon@gmail.com>
2024-03-17 12:26:55 +0800

    misc: enable cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 declare const module: any;
 
@@ -8,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  app.use(cookieParser());
 
   //Swagger Documentation
   const config = new DocumentBuilder()
